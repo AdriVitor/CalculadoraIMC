caab4691802296a35975bf4568b21cf7461f8a85
AdriVitor <advg1907@gmail.com>
2021-12-23 23:15:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CalculadoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Calculadora de IMC");
+            Console.WriteLine("Digite o seu peso: ");
+            float peso = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a sua altura: ");
+            float altura = float.Parse(Console.ReadLine());
+
+            float resultado = peso / (altura * altura);
+            
+            //Console.Clear();
+            
+            if(resultado < 18.5){
+                Console.WriteLine("Resultado: " + resultado.ToString("F"));
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if((resultado >= 18.5) && (resultado <= 24.9)){
+                Console.WriteLine("Resultado: " + resultado.ToString("F"));
+                Console.WriteLine("Peso Normal");
+            }
+            else if((resultado == 25) && (resultado <= 29.9)){
+                Console.WriteLine("Resultado: " + resultado.ToString("F"));
+                Console.WriteLine("Sobrepeso");
+            }
+            else if((resultado == 30) && (resultado <= 34.9)){
+                Console.WriteLine("Resultado: " + resultado.ToString("F"));
+                Console.WriteLine("Obesidade Grau 1");
+            }
+            else if((resultado == 35) && (resultado <= 39.9)){
+                Console.WriteLine("Resultado: " + resultado.ToString("F"));
+                Console.WriteLine("Obesidade Grau 2");
+            }
+            else if(resultado > 40){
+                Console.WriteLine("Resultado: " + resultado.ToString("F"));
+                Console.WriteLine("Obesidade Grau 3 (Mórbida)");
+            }
+
+             Console.ReadKey();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Calculadora de IMC
+
+## Objetivo
+
+<p> Programa criado para estudos </p>
+
+## Funcionalidades
+
+<p> Calcular o IMC de uma pessoa </p>
+
+## Tecnologias Utilizadas
+
+<ul>
+<li>C#</li>
+</ul>
